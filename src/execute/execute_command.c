a36c7744216a008600c95520f00151698ac823f2
pyciu123 <kuba.pyciu@gmail.com>
2025-01-14 19:29:52 +0100

    ok

--- src/execute/execute_command.c
+++ src/execute/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: kubapyciarz <kubapyciarz@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 22:39:50 by kubapyciarz       #+#    #+#             */
-/*   Updated: 2025/01/14 19:12:45 by kubapyciarz      ###   ########.fr       */
+/*   Updated: 2025/01/14 19:29:43 by kubapyciarz      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,11 +83,9 @@ int	execute_commands(t_shell *shell, t_token **tokens)
 	}
 	if (ft_strcmp(shell->segment->command, "exit") == 0)
 		return (2);
-	if (!shell->segment->relation && is_parent_builtin(shell->segment->command) == 1)
-	{
+	if (!shell->segment->relation
+		&& is_parent_builtin(shell->segment->command) == 1)
 		printf("\nbreakpoing\n\n");
-		execute_parent_builtin(shell);
-	}
 	else
 		execute_segments(shell);
 	free_segments(shell->segment);
